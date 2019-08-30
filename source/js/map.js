9fbba3e69dfe11778d5d7ccc21d9cca38ff5bced
Ostretsova Anna <ostretsova.ann@gmail.com>
2019-08-30 22:09:59 +0300

    Доделала проект

--- source/js/map.js
+++ source/js/map.js
@@ -1,5 +1,7 @@
 var newMap = document.querySelector(".address__map");
 var closeMap = document.querySelector(".address__image");
+var wrapMap = document.querySelector(".address__wrapper");
 
+wrapMap.classList.add("address__wrapper--show");
 closeMap.classList.add("address__image--close");
 newMap.classList.add("address__map--open");
